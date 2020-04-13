eedf74926358b487a24a8eaee92f8a0e2c973fe9
cagandhi <cagandhi97@gmail.com>
2020-04-13 02:27:16 -0400

    included shipping cost in total revenue query

--- Reports.java
+++ Reports.java
@@ -54,24 +54,6 @@ public class Reports
 	public void op1_get_monthly_reports_pub_bought_per_month() throws SQLException{
 		String query = "select distId,distName,pubId,title,sum(quantity) as quantity,sum(price * quantity) as total_price, extract(year from orderDate) as orderYear,extract(month from orderDate) orderMonth from Publications natural join OrderItems natural join OrderContains natural join Orders natural join Locations natural join Distributors group by distId,pubId,orderYear,orderMonth";
 		rs = statement.executeQuery(query);
-
-		/*
-		int cnt = 1;
-		while(rs.next())
-		{
-			System.out.println("\nRECORD "+cnt+": ");
-			System.out.println("Distributor ID: "+rs.getInt("distId"));
-			System.out.println("Distributor Name: "+rs.getString("distName"));
-			System.out.println("Publication ID: "+rs.getInt("pubId"));
-			System.out.println("Title: "+rs.getString("title"));
-			System.out.println("Quantity: "+rs.getInt("quantity"));
-			System.out.println("Total Price: "+rs.getFloat("total_price"));
-			System.out.println("Order Year: "+rs.getString("orderYear"));
-			System.out.println("Order Month: "+rs.getString("orderMonth"));
-			cnt++;
-		}
-		*/
-
 		
 		TableGenerator tableGenerator = new TableGenerator();
 		List<String> headersList = new ArrayList<>(); 
@@ -102,17 +84,30 @@ public class Reports
 		}
 
 		System.out.println(tableGenerator.generateTable(headersList, rowsList));
-		
-
 	}
 
 	public void op1_get_monthly_reports_total_revenue() throws SQLException{	
-		String query = "select sum(price*quantity) as total_revenue from OrderItems natural join OrderContains";
+		String query = "select sum(price*quantity) as 'totalOrder', sum(shippingCost) as 'totalShipping', sum((price*quantity)+shippingCost) as 'totalRevenue' from OrderItems natural join OrderContains natural join Orders";
 		rs = statement.executeQuery(query);
 
 		rs.next();
 
-		System.out.println("\nTotal Revenue: "+rs.getString("total_revenue"));
+		TableGenerator tableGenerator = new TableGenerator();
+		List<String> headersList = new ArrayList<>(); 
+		headersList.add("Total Order Cost");
+		headersList.add("Total Shipping Cost");
+		headersList.add("Total Revenue");
+
+		List<List<String>> rowsList = new ArrayList<>();
+
+		List<String> row = new ArrayList<>();
+		row.add(String.valueOf(rs.getFloat("totalOrder")));
+		row.add(String.valueOf(rs.getFloat("totalShipping")));
+		row.add(String.valueOf(rs.getFloat("totalRevenue")));
+
+		rowsList.add(row);
+
+		System.out.println(tableGenerator.generateTable(headersList, rowsList));
 	}
 
 	public void op1_get_monthly_reports_total_expenses() throws SQLException{
@@ -170,17 +165,6 @@ public class Reports
 		String query = "select sum(price*quantity) as total_revenue,city from OrderItems natural join OrderContains natural join Orders natural join Locations group by city";
 		rs = statement.executeQuery(query);
 
-		/*
-		int cnt = 1;
-		while(rs.next())
-		{
-			System.out.println("\nRECORD "+cnt+": ");
-			System.out.println("Total Revenue: "+rs.getFloat("total_revenue"));
-			System.out.println("City: "+rs.getString("city"));
-			cnt++;
-		}
-		*/
-
 		TableGenerator tableGenerator = new TableGenerator();
 		List<String> headersList = new ArrayList<>(); 
 
@@ -204,18 +188,6 @@ public class Reports
 	public void op3_calculate_total_revenue_per_distributor() throws SQLException{
 		String query = "select sum(price*quantity) as total_revenue,distId,distName from OrderItems natural join OrderContains natural join Orders natural join Locations natural join Distributors group by distId";
 		rs = statement.executeQuery(query);
-
-		/*
-		int cnt = 1;
-		while(rs.next())
-		{
-			System.out.println("\nRECORD "+cnt+": ");
-			System.out.println("Total Revenue: "+rs.getFloat("total_revenue"));
-			System.out.println("Distributor ID: "+rs.getInt("distId"));
-			System.out.println("Distributor Name: "+rs.getString("distName"));
-			cnt++;
-		}
-		*/
 
 		TableGenerator tableGenerator = new TableGenerator();
 		List<String> headersList = new ArrayList<>(); 
@@ -243,18 +215,6 @@ public class Reports
 		String query = "select sum(price*quantity) as total_revenue,locId,contactPerson from OrderItems natural join OrderContains natural join Orders natural join Locations group by locId";
 		rs = statement.executeQuery(query);
 
-		/*
-		int cnt = 1;
-		while(rs.next())
-		{
-			System.out.println("\nRECORD "+cnt+": ");
-			System.out.println("Total Revenue: "+rs.getFloat("total_revenue"));
-			System.out.println("Location ID: "+rs.getInt("locId"));
-			System.out.println("Contact Person Name: "+rs.getString("contactPerson"));
-			cnt++;
-		}
-		*/
-
 		TableGenerator tableGenerator = new TableGenerator();
 		List<String> headersList = new ArrayList<>(); 
 
@@ -281,17 +241,6 @@ public class Reports
 	public void op4_calculate_total_payments(String beginDate, String endDate) throws SQLException{
 		String query = "select sum(amount) as total_payment, 'Editorial Work' as work_type from (	select * from Payrolls 	where paymentDate<='"+endDate+"' 	and paymentDate>'"+beginDate+"') as T1 natural join Editors union select sum(amount) as total_payment, 'Book Authorship' as work_type from (	select * from Payrolls 	where paymentDate<='"+endDate+"' 	and paymentDate>'"+beginDate+"') as T2 natural join Authors union select sum(amount) as total_payment, 'Article Authorship' as work_type from (	select * from Payrolls 	where paymentDate<='"+endDate+"' 	and paymentDate>'"+beginDate+"') as T3 natural join Journalists";
 		rs = statement.executeQuery(query);
-
-		/*
-		int cnt = 1;
-		while(rs.next())
-		{
-			System.out.println("\nRECORD "+cnt+": ");
-			System.out.println("Total Payment: "+rs.getFloat("total_payment"));
-			System.out.println("Work Type: "+rs.getString("work_type"));
-			cnt++;
-		}
-		*/
 
 		TableGenerator tableGenerator = new TableGenerator();
 		List<String> headersList = new ArrayList<>(); 
